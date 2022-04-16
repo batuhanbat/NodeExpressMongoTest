76d0135532bcd6a23eac161c6053a7ddbb05e5eb
batuhanbat <batuhan.bat@gmail.com>
2022-04-16 15:25:53 +0300

    second endpoint
    second endpoint done, all ;'s are removed and some 	explanatory commenting done

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 
-var host = '';
-var username = '';
-var password = '';
-var port = '';
-var dbname = '';
+// SET THE CONNECTION STRING PROPERLY
 
-var fs = require('fs');
-var array = fs.readFileSync("C:/db_information.txt").toString().split("\n");
+var host = ''
+var username = ''
+var password = ''
+var port = ''
+var dbname = ''
+
+//read the db credentials file
+var fs = require('fs')
+var array = fs.readFileSync("C:/db_information.txt").toString().split("\n")
 for(i in array) {
     if (i == 0) {
-        host += array[i];
+        host += array[i]
     } else if (i == 1) {
-        username += array[i];
+        username += array[i]
     } else if (i == 2) {
-        password += array[i];
+        password += array[i]
     } else if (i == 3) {
-        port += array[i];
+        port += array[i]
     } else if (i == 4) {
-        dbname += array[i];
+        dbname += array[i]
     }
 }
 
-var connstr = "mongodb://";
-connstr += username.replace(/(\r\n|\n|\r)/gm, "") + ":";
-connstr += password.replace(/(\r\n|\n|\r)/gm, "") + "@";
-connstr += host.replace(/(\r\n|\n|\r)/gm, "") + ":";
-connstr += port.replace(/(\r\n|\n|\r)/gm, "") + "/";
-connstr += dbname.replace(/(\r\n|\n|\r)/gm, "") + "?";
-connstr += "authSource=admin";
+var connstr = "mongodb://"
+connstr += username.replace(/(\r\n|\n|\r)/gm, "") + ":"
+connstr += password.replace(/(\r\n|\n|\r)/gm, "") + "@"
+connstr += host.replace(/(\r\n|\n|\r)/gm, "") + ":"
+connstr += port.replace(/(\r\n|\n|\r)/gm, "") + "/"
+connstr += dbname.replace(/(\r\n|\n|\r)/gm, "") + "?"
+connstr += "authSource=admin"
 
-mongoose.connect(connstr);
+//connect to db
+mongoose.connect(connstr)
 
-const Schema = mongoose.Schema;
+// schema and model setup
+const Schema = mongoose.Schema
 const countrySchema = new Schema({
     name: {type:String},
     region: {type:String}
@@ -42,14 +47,50 @@ const countrySchema = new Schema({
 const Country = mongoose.model('Country', countrySchema)
 
 var allCountries = []
+var allRegions = {}
+var salesRep = []
+
 Country.find({}).then((countries) => {
-    countries.forEach(country => allCountries.push({"name":country.name, "region":country.region}));    
+    countries.forEach(
+        country => { 
+            if (country.name !== undefined && country.name !== null && country.name !== '') {
+                if (country.region !== undefined && country.region !== null && country.region !== '') {
+                    allCountries.push({"name":country.name, "region":country.region})
+                }
+            }                         
+        }
+    )
+    countries.forEach(
+        country => {
+            if (allRegions.hasOwnProperty(country.region)){
+                allRegions[country.region] += 1
+            }else{
+                allRegions[country.region] = 1
+            }
+        }
+    )  
+    keysList = Object.keys(allRegions)
+    keysList.forEach(
+        key => {
+            toPush = {}
+            toPush["region"] = key
+            toPush["minSalesReq"] = Math.ceil(allRegions[key] / 7) 
+            toPush["maxSalesReq"] = Math.ceil(allRegions[key] / 3) 
+            salesRep.push(toPush)
+        }
+    )
 }).catch( (e) => {
     console.log(e)
 })
 
+// 1st endpoint
 app.get( '/countries', (req,res) => 
     res.send(allCountries)
+)
+
+// 2nd endpoint
+app.get( '/salesrep', (req,res) => 
+    res.send(salesRep)
 )
 
 app.listen(3000)
