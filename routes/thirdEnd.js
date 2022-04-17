efe90e6e8765293853f5458f6749895d420c6fdd
batuhanbat <batuhan.bat@gmail.com>
2022-04-18 02:15:48 +0300

    last two ends now making req to first one
    with this feature, now you can send req directly to last 2 ends without sending a req to 1st beforehand

--- routes/thirdEnd.js
+++ routes/thirdEnd.js
@@ -1,9 +1,21 @@
 const express = require("express")
 let router = express.Router()
 const helperMethods = require("../helperMethods")
+const http = require('http')
+const options = {
+    host: '127.0.0.1',
+    port: 3000,
+    path: '/countries',
+    method: 'GET'
+}  
 
 // 3rd endpoint
 router.get( '/', (req,res) => {
+    const requ = http.request(options)
+    requ.on('error', error => {
+        console.error(error)    
+    })
+    requ.end()
     helperMethods.fillOptimal()
     res.send(helperMethods.optimal)
 })
