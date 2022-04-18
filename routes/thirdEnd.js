b56de70e54b6baafa5510be1b5ecc3a4f2d079d6
batuhanbat <batuhan.bat@gmail.com>
2022-04-18 03:14:57 +0300

    for all 3 endpoint requests await for 1 sec added
    After the server starts, when we get the very first req to first endpoint, we had to wait empty for 1 second just to finish connecting and reading db operations
    The other two endpoints also make a req to first endpoint so they have to wait always as well(only for a second on each req)

--- routes/thirdEnd.js
+++ routes/thirdEnd.js
@@ -17,7 +17,7 @@ router.get( '/', async function(req,res) {
             console.error(error)    
         })
         requ.end()
-        //await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 1000));
     } catch (err) {
         console.log(err)
     }
